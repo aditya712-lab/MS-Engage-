0e924b69419dbd88640c2423996a140ccd1e4d8f
aditya712-lab <72649636+aditya712-lab@users.noreply.github.com>
2022-05-30 10:57:09 +0530

    Add files via upload

---
+++ client/src/components/TopBar/Filteropns.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { MenuItem } from "@mui/material";
+
+const Filteropns = ({handleClose, setGenres, genreslist, genres}) => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  
+  return (
+    <>
+     
+        {genreslist.map((obj) => (
+          <MenuItem
+            className={"genre-item " + (genres.includes(obj) ? 'selected': '')}
+            key={obj}
+            onClick={() => {
+            handleClose()
+            
+            setGenres(prev =>{
+              if (prev.includes(obj) === true) {
+                  return prev.filter(f => f !== obj)
+              } else {
+                return [...prev, obj]
+              }
+              }
+            )
+          }} disableRipple>{obj}</MenuItem>
+        ))}
+     
+    </>
+  );
+};
+
+export default Filteropns;
---
+++ client/src/components/card/Card.jsx
@@ -0,0 +1,103 @@
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { SettingsInputComponentSharp } from "@mui/icons-material";
+import "./card.css";
+import CardMedia from "@mui/material/CardMedia";
+import { sizing } from "@mui/system";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import PlayCircleFilledWhiteIcon from "@mui/icons-material/PlayCircleFilledWhite";
+import Overlay from "./overlay/Overlay";
+
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+export default function Cards({ Id, setCurr }) {
+  const [movie, setMovie] = useState(null);
+  const [playc, setPlayc] = useState(false);
+  const [vid, setVid] = useState("");
+  const api_key = "e8f79f8cb808ed297aa4e79d8b8c277f";
+  const getmovies = async () => {
+    try {
+      const res = await axios.get(
+        `https://api.themoviedb.org/3/movie/${Id}?api_key=` + api_key
+      );
+      const vidfetch = await axios.get(
+        `https://api.themoviedb.org/3/movie/${Id}/videos?api_key=` + api_key
+      );
+
+      setVid(vidfetch.data.results[0].key);
+      setMovie(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    getmovies();
+  }, [Id]);
+  ////////////////////////////
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <>
+      <span className="card-cont">
+        <Card sx={{ maxWidth: 190, cursor: "pointer" }}>
+          <img
+            className="poster"
+            src={
+              movie?.poster_path === undefined
+                ? "https://www.bradfordbuilt.com/uploads/8/6/9/0/86908622/al-ss-loading_orig.jpg"
+                : "https://image.tmdb.org/t/p/w300" + movie?.poster_path
+            }
+          ></img>
+        </Card>
+
+        <div className="fav" typeof="text">
+          {movie?.genres[0].name}
+          {/* <FavoriteBorderIcon
+            sx={{ marginLeft: "15px", color: "red", cursor: "pointer" }}
+          /> */}
+          <div
+            className="play"
+            onClick={() => {
+              movie && setCurr(movie);
+            }}
+          >
+            <div className="playbtn">
+              <PlayCircleFilledWhiteIcon
+                sx={{ marginLeft: "15px", color: "red", cursor: "pointer" }}
+              />
+            </div>
+          </div>
+        </div>
+      </span>
+    </>
+  );
+}
